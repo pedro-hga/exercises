79a20c53ae40c0ed6d92dd81cd820a65d1fd3b72
Pedro Henrique <73372271+pedro-hga@users.noreply.github.com>
2024-06-07 17:20:38 -0300

    added ruby exercism exercises

---
+++ exercism/php/windowing-system/ProgramWindow.php
@@ -0,0 +1,15 @@
+<?php
+
+class ProgramWindow
+{
+  public $x;
+  public $y;
+  public $height;
+  public $width;
+   function __construct() {
+    $this->$x = 0;
+    $this->$y = 0;
+    $this->$width = 800;
+    $this->$height = 600;
+  }
+}
---
+++ exercism/php/windowing-system/Size.php
@@ -0,0 +1,9 @@
+<?php
+
+class Size
+{
+  public $x;
+  public $y;
+  public $height;
+  public $width;
+}
