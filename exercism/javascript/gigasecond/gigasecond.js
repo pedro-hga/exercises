2db60e3643a7c8a61521fb0573a5352481215520
Pedro <pedro_hga@proton.me>
2025-10-09 14:17:17 -0300

    sync

---
+++ exercism/javascript/gigasecond/gigasecond.js
@@ -0,0 +1,6 @@
+export const gigasecond = (date) => {
+	const parsed = Date.parse(date);
+	console.log(parsed);
+	// add gigasecond (1000000000000) -> in milliseconds, so there's no conversion to seconds
+	// convert back to date
+};
