ce774689673ada156af600c832e7571ce64e9c0a
Pedro <pedro_hga@proton.me>
2026-04-29 10:08:19 -0300

    exercism/ruby/acronym

--- exercism/ruby/acronym/acronym.rb
+++ exercism/ruby/acronym/acronym.rb
@@ -1,7 +1,5 @@
-=begin
-Write your code for the 'Acronym' exercise in this file. Make the tests in
-`acronym_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/acronym` directory.
-=end
+class Acronym
+  def self.abbreviate(string)
+    string.split(/[^a-zA-Z]+/).reject(&:empty?).map { |word| word[0].upcase }.join
+  end
+end
